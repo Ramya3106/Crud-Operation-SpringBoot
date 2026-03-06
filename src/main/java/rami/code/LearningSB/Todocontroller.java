79d8ea4d445b4193e65306e4162b97d85f868fda
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-06 19:28:15 +0530

    installed maven

--- src/main/java/rami/code/LearningSB/Todocontroller.java
+++ src/main/java/rami/code/LearningSB/Todocontroller.java
@@ -1,16 +1,20 @@
 package rami.code.LearningSB;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/todo")
 public class Todocontroller {
-    @GetMapping("/todo/get")
+    @GetMapping("/get")
         String getTodo(){
             return "Todo";
     }
-    @GetMapping("/todo/id")
-    String getTodoById(){
-        return "Todo with Id";
+    //PathVariable
+    @GetMapping("/{id}")
+    String getTodoById(@PathVariable long id){
+        return "Todo with Id" + id;
     }
 }
