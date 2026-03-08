8b69218938d58bba0f01c894c926d50741d040c2
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-08 20:09:04 +0530

    updated

--- src/main/java/rami/code/LearningSB/models/Todo.java
+++ src/main/java/rami/code/LearningSB/models/Todo.java
@@ -1,9 +1,13 @@
 package rami.code.LearningSB.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Todo {
+    @Id
+    @GeneratedValue
 long id;
 String title;
 String description;
