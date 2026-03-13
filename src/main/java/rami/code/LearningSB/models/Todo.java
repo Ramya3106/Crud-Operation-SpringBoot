9fe129c536a1b91b9aca0040a14525c5df14361e
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-13 20:57:03 +0530

    :updated

--- src/main/java/rami/code/LearningSB/models/Todo.java
+++ src/main/java/rami/code/LearningSB/models/Todo.java
@@ -3,7 +3,12 @@ package rami.code.LearningSB.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NonNull;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.NotFound;
 
 @Entity
 @Data
@@ -11,7 +16,13 @@ public class Todo {
     @Id
     @GeneratedValue
 Long id;
+    @NotNull
+    @NotBlank
 String title;
+    @NotNull
+    @NotBlank
 String description;
 Boolean isCompleted;
+@Email
+String email;
 }
