31b4f28ea5ef3c7fedc56b1608ff479ee17756ed
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-14 12:00:21 +0530

    updated

--- src/main/java/rami/code/LearningSB/SecurityConfig.java
+++ src/main/java/rami/code/LearningSB/SecurityConfig.java
@@ -2,6 +2,7 @@ package rami.code.LearningSB;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +15,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth) -> auth
                         .anyRequest().authenticated()
-                );
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
