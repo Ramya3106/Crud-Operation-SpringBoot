1de1894e786358046d7c93e3eebed686539c892d
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-14 13:50:45 +0530

    updated

--- src/main/java/rami/code/LearningSB/utils/JwtUtil.java
+++ src/main/java/rami/code/LearningSB/utils/JwtUtil.java
@@ -1,9 +1,15 @@
 package rami.code.LearningSB.utils;
 
+
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 
 @Component
 public class JwtUtil {
-    private final String SECERT = "";
-    private final long EXPIRATION = 1000*300;
+    private final String SECERT = "code io";
+    private final long EXPIRATION = 1000*60;
+    private final Key secertKey = Keys.hmacShaKeyFor(SECERT.getBytes(StandardCharsets.UTF_8));
 }
