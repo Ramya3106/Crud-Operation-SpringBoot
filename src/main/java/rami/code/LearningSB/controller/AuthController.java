34a5f0d50086e575431f68552df5133958dec46d
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-14 13:22:28 +0530

    updated

--- src/main/java/rami/code/LearningSB/controller/AuthController.java
+++ src/main/java/rami/code/LearningSB/controller/AuthController.java
@@ -1,4 +1,19 @@
 package rami.code.LearningSB.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class AuthController {
+    @PostMapping("/register")
+    public String registerUser(@RequestBody Map<String, String> body){
+
+    }
+    @PostMapping("/login")
+    public String loginUser(@RequestBody Map<String, String> body){
+
+    }
 }
---
+++ src/main/java/rami/code/LearningSB/utils/JwtUtil.java
@@ -0,0 +1,9 @@
+package rami.code.LearningSB.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    private final String SECERT = "";
+    private final long EXPIRATION = 1000*300;
+}
