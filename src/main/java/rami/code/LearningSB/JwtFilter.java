ed50d8fa10d088db6c526883a7de1eae66f04933
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-15 17:52:38 +0530

    updated

--- src/main/java/rami/code/LearningSB/JwtFilter.java
+++ src/main/java/rami/code/LearningSB/JwtFilter.java
@@ -4,15 +4,33 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import rami.code.LearningSB.utils.JwtUtil;
 
 import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+    private final StringHttpMessageConverter stringHttpMessageConverter;
+
+    public JwtFilter(StringHttpMessageConverter stringHttpMessageConverter) {
+        this.stringHttpMessageConverter = stringHttpMessageConverter;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
 
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            if (jwtUtil.validateJwtToken(token)) {
+                String email =
+            }
+        }
     }
 }
